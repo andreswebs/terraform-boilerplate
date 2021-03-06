bec7fce524f88c859759b9a7f110e9530b25e05b
andreswebs <andresil@pm.me>
2021-03-06 11:38:16 -0300

    upgrade tf 0.14

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,2 +1,21 @@
+terraform {
+
+  required_version = ">= 0.14"
+
+  backend "s3" {
+    bucket = "infrastructure.terraform.state"
+    key    = "environments/dev.tfstate"
+    region = "us-east-1"
+  }
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
 
 
